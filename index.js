c7467afd3105382bb9d47b8086b8a4f9e4cd755f
Komefumi <komefumi@protonmail.ch>
2021-12-12 21:41:12 +0530

    add dummy index.js to comply with listed main property in package.json

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`
+x-ember-cli is meant to be used as a cli application.
+It's used globally. Just run \`x-ember-cli\` and pass any commands you need that would be run by the ember-cli`);
+
+console.log(`
+Note that x-ember-cli is limited.
+Use for only the purposes intended,
+and only use if your version of ember-cli is close to the version listed in the peer dependencies of this package`);
